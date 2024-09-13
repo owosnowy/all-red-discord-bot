d1350e698f3799d8e14915efc9f5fc71740b14c5
snowyluv <snowyfe@proton.me>
2024-09-12 22:49:58 -0500

    working on cmd handler

--- src/bot/client.ts
+++ src/bot/client.ts
@@ -1,12 +1,19 @@
 import { Client, Events, GatewayIntentBits, Collection } from 'discord.js';
+import fg from 'fast-glob';
+import type { ApplicationCommandData } from "discord.js";
 let client = new Client({intents: [GatewayIntentBits.Guilds]});
 client.once(Events.ClientReady, (c) => {
   console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 class Bot extends Client {
+    public slashdata: ApplicationCommandData[] = [];
     public async start(): Promise<void> {
 this.login(process.env.DISCORD_TOKEN);
 this.setMaxListeners(0);
+this.slashdata = [];
+let command_files: string[] = await fg(
+   `${__dirname.replace(/\\/g, '/')}/../commands/**/*{.ts,.js}`
+)
 this.once(Events.ClientReady, (c) => {
   console.log(`Logged in as ${c.user.username}`);
 });
